dc0e1bc4ddbb4a175a54ed6f68f25a9bf63728b0
addemark <addemark@gmail.com>
2023-02-12 20:25:22 +0200

    test

--- cloud/triggers/user.js
+++ cloud/triggers/user.js
@@ -1,6 +1,6 @@
-Parse.Cloud.beforeSave('_User', (request, response) => {
-  if (!request.object.get('password')) {
-    response.error('must contain password');
+Parse.Cloud.beforeSave("_User", (request, response) => {
+  if (!request.object.get("password")) {
+    response.error("must contain password.");
   }
   response.success();
 });
